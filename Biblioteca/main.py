0b3e93078584f592e2659bee507b2861498da138
Toma Augustina <52568962+Atuogmua@users.noreply.github.com>
2024-05-16 09:38:24 +0300

    recenzie - stele/ json

--- Biblioteca/main.py
+++ Biblioteca/main.py
@@ -2,21 +2,28 @@ import json
 import pandas as pd
 import streamlit as st
 import numpy as np
-
+from streamlit_star_rating import st_star_rating
 
 st.header("Biblioteca persoanala")
-tab = pd.DataFrame(columns=("Denumire","Autor","Recenzie proprie"))
+row1 = pd.DataFrame(columns=("Denumire","Autor","Recenzie proprie"))
+tab = st.table(row1)
 
-st.dataframe(tab)  # Same as st.write(df)
 buton = st.button("Add new row")
-
 st.write("value:", buton)
 if buton == True:
-    st.write(":blue[a]")
-    denumire = st.text_input("Denumire")
-    autor = st.text_input("Autor")
-    rec_prop =
-    df = pd.DataFrame(columns = (denumire, autor, rec_prop))
-    #tab.add_row("Denumire":denumire)
-    buton = False
+    denumire = st.text_area("Denumire")
+    autor = st.text_area("Autor")
+    #rec_prop = st_star_rating("Recenzie proprie", maxValue=5, defaultValue=3, key="rating")
+    df = pd.DataFrame(columns = (denumire, autor))
+    tab.add_row(df)
+    new_row = {
+        "Denumire": str(denumire),
+        "Autor": str(autor)
+        #"Recenzii proprii": int(rec_prop)
+    }
+    biblio = json.dumps(new_row, sort_keys = True)
+    ver = st.button("Gata")
+    if ver == True:
+        buton = False
+
 st.write("value:", buton)
