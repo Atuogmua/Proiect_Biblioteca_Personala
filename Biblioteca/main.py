8672e867df0e95928fb7d46acedaac3079136e53
Toma Augustina <52568962+Atuogmua@users.noreply.github.com>
2024-05-16 08:59:12 +0300

    add button+create table

--- Biblioteca/main.py
+++ Biblioteca/main.py
@@ -1,4 +1,12 @@
 import json
+import pandas as pd
 import streamlit as st
-st.write("Hello World!")
-st.dataframe()
+st.header("Biblioteca persoanala")
+tab =st.dataframe()
+buton = st.button("Add new row")
+
+st.write("value:", buton)
+if buton == True:
+    st.write("a")
+    buton = False
+i2 = st.checkbox("reset button")
