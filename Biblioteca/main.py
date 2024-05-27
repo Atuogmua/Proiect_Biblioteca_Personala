fa769b98da8bfd4c9f60add40cbd31edb9df8de5
Toma Augustina <52568962+Atuogmua@users.noreply.github.com>
2024-05-27 16:44:02 +0300

    add to row updates

--- Biblioteca/main.py
+++ Biblioteca/main.py
@@ -4,26 +4,24 @@ import streamlit as st
 import numpy as np
 from streamlit_star_rating import st_star_rating
 
-st.header("Biblioteca persoanala")
-row1 = pd.DataFrame(columns=("Denumire","Autor","Recenzie proprie"))
+st.title("Biblioteca personală")
+
+if "row_data" not in st.session_state:
+    st.session_state.row_data = []
+
+row1 = pd.DataFrame(st.session_state.row_data, columns=["Denumire", "Autor", "Recenzie proprie"])
 tab = st.table(row1)
 
-buton = st.button("Add new row")
-st.write("value:", buton)
-if buton == True:
-    denumire = st.text_area("Denumire")
-    autor = st.text_area("Autor")
-    #rec_prop = st_star_rating("Recenzie proprie", maxValue=5, defaultValue=3, key="rating")
-    df = pd.DataFrame(columns = (denumire, autor))
-    tab.add_row(df)
-    new_row = {
-        "Denumire": str(denumire),
-        "Autor": str(autor)
-        #"Recenzii proprii": int(rec_prop)
-    }
-    biblio = json.dumps(new_row, sort_keys = True)
-    ver = st.button("Gata")
-    if ver == True:
-        buton = False
+with st.expander("**Add New Row**"):
+    den = st.text_input("Denumire", placeholder="Introduceti denumirea carții")
+    aut = st.text_input("Autor", placeholder="Introduceti numele autorului")
+    rec_prop = st_star_rating("Recenzie proprie", maxValue=5, defaultValue=0)
 
-st.write("value:", buton)
+    if st.button("Gata"):
+        if den and aut:
+            new_row = {"Denumire": den, "Autor": aut, "Recenzie proprie": rec_prop}
+            st.session_state.row_data.append(new_row)
+            row1 = pd.DataFrame(st.session_state.row_data)
+            st.experimental_rerun()
+
+st.write(st.session_state)
